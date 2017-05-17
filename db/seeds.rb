7ccc8222bea9c6408cf3df9f06232def7c5da6c8
annbelievable <kokann_92@hotmail.com>
2017-05-17 20:55:42 +0800

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+status = ['done','undone','started','on going','few mistakes','fixed']
+
+ActiveRecord::Base.transaction do
+  10.times do
+    task = {}
+    task['title'] = Faker::Name.first_name
+    task['description'] = Faker::Internet.email
+    task['status'] = status.sample
+    Task.create(task)
+  end
+end
+
+ActiveRecord::Base.transaction do
+  50.times do
+    subtask = {}
+    subtask['title'] = Faker::Address.city
+    subtask['description'] = Faker::Address.street_address
+    subtask['status'] = status.sample
+    subtask['task_id'] = rand(1..10)
+    Subtask.create(subtask)
+  end
+end
